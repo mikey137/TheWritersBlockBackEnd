fc4ccf4376d1f460f77ae96dcb373bc06e6c9b30
MichaelHulme <coachhulmeumb@gmail.com>
2022-05-10 13:54:46 -0400

    changed origin

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ console.log(originUrl)
 app.use(express.json())
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(cors({
-    origin: '*',
+    origin: 'https://master--radiant-axolotl-de1247.netlify.app',
     credentials: true,
 }));
 
