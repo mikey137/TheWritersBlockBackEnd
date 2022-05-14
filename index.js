9de8d42751afb0dbc5c3120cb01659e47ea4c2d0
MichaelHulme <coachhulmeumb@gmail.com>
2022-05-14 16:18:10 -0400

    removed cors npm

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ console.log(originUrl)
 //middleware
 app.use(express.json())
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
-app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://master--radiant-axolotl-de1247.netlify.app")
+    res.header("Access-Control-Allow-Headers", "X-Requested-With")
+    next()
+});
 
 //Routes
 
