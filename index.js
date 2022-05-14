35fa6f651114a10c6fec778ae79c7390fd79676d
MichaelHulme <coachhulmeumb@gmail.com>
2022-05-14 16:05:40 -0400

    removed origin

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ console.log(originUrl)
 //middleware
 app.use(express.json())
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
-app.use(cors({
-    origin: 'https://master--radiant-axolotl-de1247.netlify.app'
-}));
+app.use(cors());
 
 //Routes
 
