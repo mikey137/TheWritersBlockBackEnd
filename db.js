3856ee63675dd323647b506a59bdcece0e6580f0
MichaelHulme <coachhulmeumb@gmail.com>
2022-05-15 11:37:40 -0400

    added env variables

--- db.js
+++ db.js
@@ -1,11 +1,21 @@
 const Pool = require('pg').Pool
+const dotenv = require('dotenv')
+dotenv.config()
+
+const DB_USER = process.env.NODE_ENV === 'development' ? process.env.DB_LOCAL_USER : process.env.DB_PROD_USER
+
+const DB_PASSWORD = process.env.NODE_ENV === 'development' ? process.env.DB_LOCAL_PASSWORD : process.env.DB_PROD_PASSWORD
+
+const DB_HOST = process.env.NODE_ENV === 'development' ? process.env.DB_LOCAL_HOST : process.env.DB_PROD_HOST
+
+const DB_DBNAME = process.env.NODE_ENV === 'development' ? process.env.DB_LOCAL_DBNAME : process.env.DB_PROD_DBNAME
 
 const pool = new Pool({
-    user: "postgres",
-    password: "password",
-    host: "localhost",
+    user: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
     port: 5432,
-    database: "jwttutorial"
+    database: DB_DBNAME
 })
 
 module.exports = pool
